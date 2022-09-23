1c05aa986204c5d0a2f004d12705bf341d2f104b
Victor Kane <victorkane@gmail.com>
2022-09-23 13:35:42 -0300

    updated blog post layout page to latest version of fresh

--- routes/blog/[id].tsx
+++ routes/blog/[id].tsx
@@ -1,23 +1,21 @@
-import { h } from "preact";
-import { tw } from "twind";
 import { Post } from "../../utils/posts.ts";
 
 const post: Post = {
-  id: "hello",
-  title: "Hello World",
+  id: "eat-and-sleep",
+  title: "Eat and Sleep",
   publishAt: new Date("2020-01-01"),
-  snippet: "The first post.",
-  content: "Hello World",
+  snippet: "The second post.",
+  content: "Eat and sleep is...",
 };
 
 export default function PostPage() {
   return (
-    <div class={tw`px-4 mx-auto max-w-screen-md`}>
-      <p class={tw`text-gray-600 mt-12`}>
+    <div class="px-4 mx-auto max-w-screen-md">
+      <p class="text-gray-600 mt-12">
         {post.publishAt.toLocaleDateString()}
       </p>
-      <h1 class={tw`font-bold text-5xl mt-2`}>{post.title}</h1>
-      <div class={tw`mt-12`}>{post.content}</div>
+      <h1 class="font-bold text-5xl mt-2">{post.title}</h1>
+      <div class="mt-12">{post.content}</div>
     </div>
   );
 }
