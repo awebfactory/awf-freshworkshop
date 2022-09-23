9d387ee9b511a6ebcc23e09cc8ea02a4caadd400
Victor Kane <victorkane@gmail.com>
2022-09-23 12:02:41 -0300

    home: minor edits

--- routes/index.tsx
+++ routes/index.tsx
@@ -9,8 +9,7 @@ export default function Home() {
         alt="the fresh logo: a sliced lemon dripping with juice"
       />
       <p class="my-6">
-        Welcome to Victor's `fresh` example app. Try updating this message in
-        the ./routes/index.tsx file, and refresh.
+        Hello, worlds!
       </p>
       <Counter start={3} />
     </div>
