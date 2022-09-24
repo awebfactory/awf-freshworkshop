778444f2ebe29e0b2e3f005cd5c7718eefe7094b
Victor Kane <victorkane@gmail.com>
2022-09-24 12:15:35 -0300

    Render dates based on user languages using middleware parsing users preferred locale from the accept-language header, format date accordingly

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,14 +3,16 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/blog/[id].tsx";
-import * as $1 from "./routes/index.tsx";
+import * as $0 from "./routes/_middleware.ts";
+import * as $1 from "./routes/blog/[id].tsx";
+import * as $2 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
-    "./routes/blog/[id].tsx": $0,
-    "./routes/index.tsx": $1,
+    "./routes/_middleware.ts": $0,
+    "./routes/blog/[id].tsx": $1,
+    "./routes/index.tsx": $2,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,22 +1,27 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
 import { listPosts, Post } from "../utils/posts.ts";
 import Counter from "../islands/Counter.tsx";
+import { State } from "../utils/state.ts";
 
-export const handler: Handlers<Post[]> = {
+interface Data extends State {
+  posts: Post[];
+}
+
+export const handler: Handlers<Data, State> = {
   async GET(_req, ctx) {
     const posts = await listPosts();
-    return ctx.render(posts);
+    return ctx.render({ ...ctx.state, posts });
   },
 };
 
-export default function Home(props: PageProps<Post[]>) {
-  const posts = props.data;
+export default function Home(props: PageProps<Data>) {
+  const { posts, locales } = props.data;
   return (
     <div class="p-4 mx-auto max-w-screen-md">
       <div class="px-4 mx-auto max-w-screen-md">
         <h1 class="font-bold text-5xl mt-12">Luca's Cats' Blog</h1>
         <ul class="mt-8">
-          {posts.map((post) => <PostEntry post={post} />)}
+          {posts.map((post) => <PostEntry post={post} locales={locales} />)}
         </ul>
       </div>
       <img
@@ -32,10 +37,9 @@ export default function Home(props: PageProps<Post[]>) {
   );
 }
 
-function PostEntry(props: { post: Post }) {
-  const { post } = props;
-  const locale = "en-US";
-  const dateFmt = new Intl.DateTimeFormat(locale, { dateStyle: "short" });
+function PostEntry(props: { post: Post; locales: string[] }) {
+  const { post, locales } = props;
+  const dateFmt = new Intl.DateTimeFormat(locales, { dateStyle: "short" });
   return (
     <li class="border-t">
       <a href={`/blog/${post.id}`} class="py-2 flex group gap-4">
