2c547edbcf6f558a1b3279281f6bdab85129d301
Victor Kane <victorkane@gmail.com>
2022-09-24 15:09:58 -0300

    Update locale via POST request (form on settings page)

--- routes/settings.tsx
+++ routes/settings.tsx
@@ -1,9 +1,22 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
+import { setCookie } from "$std/http/cookie.ts";
 import { State } from "../utils/state.ts";
 
 export const handler: Handlers<State, State> = {
   GET(_req, ctx) {
     return ctx.render({ ...ctx.state });
+  },
+  async POST(req) {
+    const form = await req.formData();
+    const locale = form.get("locale");
+    const resp = new Response("", {
+      status: 303,
+      headers: { Location: "/settings" },
+    });
+    if (typeof locale === "string") {
+      setCookie(resp.headers, { name: "locale", value: locale });
+    }
+    return resp;
   },
 };
 
@@ -15,6 +28,21 @@ export default function SettingsPage(props: PageProps<State>) {
       <p class="mt-4">
         Your current locale is <b>{locales[0]}</b>.
       </p>
+      <form action="/settings" method="post" class="mt-4">
+        <label htmlFor="locale">Locale</label>
+        <input
+          type="text"
+          name="locale"
+          id="locale"
+          class="border px-2 py-1"
+        />
+        <button
+          type="submit"
+          class="bg-blue(500 hover:700 disabled:200) text-white px-2 py-1"
+        >
+          Save
+        </button>
+      </form>
     </div>
   );
 }
